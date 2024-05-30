a21d3a9ddee38c97d93b2ef4d4abec3f7141be53
mehmetkaya1989 <167673068+mehmetkaya1989@users.noreply.github.com>
2024-05-30 17:14:23 +0300

    Create dnm.py

---
+++ dnm.py
@@ -0,0 +1,31 @@
+import cv2
+import streamlit as st
+import numpy as np
+import tempfile
+
+cap = cv2.VideoCapture(0)
+
+st.title("Opem cv Deneme")
+
+frame_placeholder = st.empty()
+stop_button = st.button("Stop")
+
+st.title("Video Yakalama")
+
+while cap.isOpened() and not stop_button:
+    ret, frame = cap.read()
+    if not ret:
+        st.write("the video capture has ended.")
+        break
+
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    frame = cv2.flip(frame, 1)
+
+    frame_placeholder.image(frame,channels="RGB")
+
+    if cv2.waitKey(1) % 0xFF == ord ("q") or stop_button:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
